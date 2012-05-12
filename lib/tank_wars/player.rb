b3b32d7a943f0614f44ff7b6bb794b02c0994399
Carl Youngblood <carl@youngbloods.org>
2012-05-12 17:41:21 +0200

    Player class can accept player numbers higher than 4

--- lib/tank_wars/player.rb
+++ lib/tank_wars/player.rb
@@ -11,7 +11,7 @@ class Player < Chingu::GameObject
     super
     @width = 50
     @height = 20
-    @player_number = options[:player_number]
+    @player_number = options[:player_number] % COLORS.length + 1
     @x = options.fetch(:x, $window.width - ($window.width / 5 * @player_number) - (@width / 2))
     @gun_base_x = @x + (@width / 2)
     @y = 600
