dc1c82e7f2b3a43f9d03e4b5f35a7cebaeae429a
Syver Enstad <syver.enstad@cisco.com>
2012-05-12 22:34:48 +0200

    Indent properly

--- lib/tank_wars/player_sound.rb
+++ lib/tank_wars/player_sound.rb
@@ -1,28 +1,28 @@
 module TankWars
-module PlayerSound
-  def play_select_sound
-    if @select_sound
-      unless @select_sound.playing?
+  module PlayerSound
+    def play_select_sound
+      if @select_sound
+        unless @select_sound.playing?
+          @select_sound = Sound["select.wav"].play
+        end
+      else
         @select_sound = Sound["select.wav"].play
       end
-    else
-      @select_sound = Sound["select.wav"].play
     end
-  end
 
-  def play_power_sound
-    if @power_sound
-      unless @power_sound.playing?
+    def play_power_sound
+      if @power_sound
+        unless @power_sound.playing?
+          @power_sound = Sound["power.wav"].play
+        end
+      else
         @power_sound = Sound["power.wav"].play
       end
-    else
-      @power_sound = Sound["power.wav"].play
     end
-  end
 
-  def play_shot_fired_sound
-    Sound["shot_fired.wav"].play
-  end
+    def play_shot_fired_sound
+      Sound["shot_fired.wav"].play
+    end
 
-end
+  end
 end
