ce5d09438719cf88a408402e6f701296b97a38a3
Syver Enstad <syver.enstad@cisco.com>
2012-05-13 12:22:38 +0200

    Try to simplify networking.rb

--- lib/tank_wars/main_window.rb
+++ lib/tank_wars/main_window.rb
@@ -1,10 +1,6 @@
-#!/usr/bin/env ruby
-
 require 'tank_wars/player'
 require 'tank_wars/background'
 require 'tank_wars/networking'
-
-OUR_PLAYER_NUMBER = 1
 
 
 module TankWars
@@ -12,16 +8,14 @@ module TankWars
     def initialize(host, port = 9876)
       super(1024, 768, false)
       self.input = {escape: :exit} # exits example on Escape
-
       @background = Background.create
-      @networking = Networking.new(host, port, self)
-
+      @network = Network.new(host, port)
       @players = {}
     end
 
     def update
       super
-      @networking.client.run
+      @network.dispatch_network_events_to(self)
       self.caption = "FPS: #{self.fps} ms since last tick: " +
           "#{self.milliseconds_since_last_tick}"
     end
@@ -42,8 +36,7 @@ module TankWars
             :player_number => id,
             :target_angle => angle,
             :is_me => (id == @self_id),
-            :networking => @networking
-        )
+            :network => @network)
       end
 
       left.each do |id|
